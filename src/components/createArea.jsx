81d274a3063b705c341bc9adb3faac70c3802384
Tanmay Gadge <tanmaygadge9273@gmail.com>
2024-08-13 20:47:10 +0530

    created keeper app

---
+++ src/components/createArea.jsx
@@ -0,0 +1,78 @@
+import React, { useState } from "react";
+import AddIcon from '@mui/icons-material/Add';
+import Fab from '@mui/material/Fab';
+import Zoom from '@mui/material/Zoom';
+
+function CreateArea(props) {
+  const [note, setNote] = useState({
+    title: "",
+    content: "",
+  });
+
+  const [isActive, setIsActive] = useState(false);
+
+  function handleChange(event) {
+    const { name, value } = event.target;
+
+    setNote((prevNote) => {
+      return {
+        ...prevNote,
+        [name]: value,
+      };
+    });
+
+    setIsActive(true);
+  }
+
+  function submitNote(event) {
+    props.onAdd(note);
+    setNote({
+      title: "",
+      content: "",
+    });
+    event.preventDefault();
+  }
+
+  function handleClick() {
+    setIsActive(true);
+  }
+
+  return (
+    <div>
+      <form className="create-note">
+        {/* <input
+          name="title"
+          onChange={handleChange}
+          value={note.title}
+          placeholder="Title"
+        /> */}
+
+        {isActive && (
+          <input
+            name="title"
+            onChange={handleChange}
+            value={note.title}
+            placeholder="Title"
+          />
+        )}
+        <textarea
+          name="content"
+          onChange={handleChange}
+          value={note.content}
+          placeholder="Take a note..."
+          rows={isActive ? "3" : "1"}
+          onClick={handleClick}
+        />
+        {isActive && (
+          <Zoom in={{ isActive }}>
+            <Fab onClick={submitNote}>
+              <AddIcon />
+            </Fab>
+          </Zoom>
+        )}
+      </form>
+    </div>
+  );
+}
+
+export default CreateArea;
